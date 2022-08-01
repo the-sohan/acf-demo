d103c87fe760f9bfe3146bc7439d95b941b0e348
Sohan Chowdhury <89298094+the-sohan@users.noreply.github.com>
2022-08-01 10:02:35 +0600

    reqired plugin function

--- acf-demo.php
+++ acf-demo.php
@@ -17,3 +17,32 @@ function acfd_boostrap(){
     load_plugin_textdomain( 'acf-demo', false, dirname(__FILE__) . "/languages" );
 }
 add_action( 'plugins_loaded', 'acfc_bootstrap' );
+
+add_action( 'tgmpa_register', 'acfd_tgm_register_required_plugins' );
+
+function acfd_tgm_register_required_plugins() {
+
+	$plugins = array(
+		array(
+			'name'      => 'Advanced Custom Fields',
+			'slug'      => 'advanced-custom-fields',
+			'required'  => true,
+		),
+	);
+
+	$config = array(
+		'id'           => 'acf-demo',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'plugins.php',            // Parent menu slug.
+		'capability'   => 'manage_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+
+	);
+
+	tgmpa( $plugins, $config );
+}
